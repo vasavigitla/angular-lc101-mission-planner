2fd1533d402875c0a9503102336837fa839f82a0
Vasavi <vasavi.gitla@yahoo.com>
2020-12-10 19:10:58 -0600

    commit

--- mission-planning-dashboard/src/app/crew/crew.component.ts
+++ mission-planning-dashboard/src/app/crew/crew.component.ts
@@ -4,12 +4,13 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-crew',
   templateUrl: './crew.component.html',
   styleUrls: ['./crew.component.css']
+
 })
 export class CrewComponent implements OnInit {
-
+  crew: string[] = ["Jessica Watkins", "Raja Chari", "Jasmin Moghbeli"]
   constructor() { }
 
   ngOnInit() {
   }
-
+  
 }
